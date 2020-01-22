3110253f81ff6a06f6b05e3c84e5923d9f3bb5df
Keshav Iyengar <dearkri03@gmail.com>
2020-01-22 15:10:24 -0500

    Added word class

---
+++ Word.java
@@ -0,0 +1,50 @@
+
+public class Word {
+	private String word;
+	
+	public Word() {
+		word = "";
+	}
+	
+	public Word(String word) {
+		this.word = word.toLowerCase();
+	}
+	
+	public int length() {
+		return word.length();
+	}
+	
+	public String getWord() {
+		return word;
+	}
+	
+	public int numSylab() {
+		return Math.max(1, numVowels() - numReductions());
+	}
+	
+	public int numVowels() {
+		int j = 0;
+		for(int i = 0; i < word.length(); i++)
+			if(word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u' || word.charAt(i) == 'y')
+				j++;
+		return j;
+	}
+	
+	public boolean isVowel(int i) {
+		return word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u' || word.charAt(i) == 'y';
+	}
+	
+	public int numReductions() {
+		int numRed = 0;
+		if(word.charAt(word.length()-1) == 'e' ||
+				((word.charAt(word.length()-3) != 'e' && word.charAt(word.length()-3) != 's') && word.substring(word.length()-2).equals("es") || (word.substring(word.length()-2).equals("ed") && word.charAt(word.length()-3) != 't')) || word.substring(word.length()-2).equals("ao"))                                      
+			numRed++;
+		
+		
+		for(int i = 0; i < word.length()-2; i++) 
+			if(isVowel(i) && isVowel(i+1)) 
+				if(!(word.charAt(i) == 'i' && (word.charAt(i+1) == 'a' || word.charAt(i+1) == 'o')) && word.charAt(i+1) != 'y')
+					numRed++;
+		return numRed;
+	}
+}
